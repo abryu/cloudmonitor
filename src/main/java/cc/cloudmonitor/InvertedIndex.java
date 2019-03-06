71491278fffa2fd3d07f270d4c2ea77afdcaa3ae
abryu082 <wenboyu2208@gmail.com>
2019-03-06 14:44:02 -0500

    travis

---
+++ src/main/java/cc/cloudmonitor/InvertedIndex.java
@@ -0,0 +1,11 @@
+package cc.cloudmonitor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InvertedIndex {
+
+  Map<String, Map<String, List<String>>> invertedIndex = new HashMap<>();
+
+}
---
+++ udass2/InvertedIndex.py
@@ -0,0 +1,14 @@
+import os
+
+# all words n the index lower case
+# no punction numbers symbos
+# stopword and but is the to
+# assume a set of files in a directory
+
+
+
+index = ()
+
+def main (directoryPath) :
+
+    for filename in os.li
